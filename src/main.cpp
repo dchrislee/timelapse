edeecb762cc0e0e9e4e1bfc3673ea45d231f049f
Dmitry Gorohov <dng@intetics.com>
2015-01-25 16:20:12 +0300

    Fixed WDT

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
 #ifdef __AVR_ATtiny45__
 #include <avr/power.h>
 #endif
 #include <avr/sleep.h>
 
+#ifndef _USE_UTIL_DELAY
+#include <util/dela.h>
+#endif
+
+#ifdef _USE_EEPROM_
+#include <avr/eeprom.h>
+uint8_t EEMEM _saved_data;
+#endif
+
 void send_pulses();
 void shoot_camera();
 void wdt_disable();
 void wdt_enable();
 void shift(uint8_t data);
+
+#define WDT_DEFAULT                         (1 << WDP2)   // 0.25 sec
 
 #define SR595_SER_DATA                      PB0	// 14 pin
 #define SR595_RCLK_LATCH                    PB1	// 12 pin
@@ -36,13 +46,17 @@ void shift(uint8_t data);
 #define MAKE_HIGH(X, Y)                     X |= (1 << Y)
 #define TOGGLE_BIT(X, Y)                    X ^= (1 << Y)
 
-#define OCR_VALUE(X, P)                     ((F_CPU / P) / X) - 1
+#ifdef _USE_EEPROM_
+uint8_t _data;
+#else
+uint8_t _data = 0xFF;
+#endif //_USE_EEPROM_
 
-uint8_t _data = 0;
 uint16_t _app_state = 0;
 uint8_t _flash_cnt = 0;
 uint8_t _flashed = 0;
 uint8_t _display_timeout = 0;
+uint16_t _program_cnt = 0;
 
 #define SET_MODE(_MODE)                     MAKE_HIGH(_app_state, _MODE)
 #define IS_MODE(_MODE)                      _app_state & (1 << _MODE)
@@ -57,16 +71,26 @@ uint8_t _display_timeout = 0;
 #define FLASHED_VALUE                       7
 #define TURN_OFF_FLASH                      8
 #define SHOOT_CAMERA                        9
+#define RUN_PROGRAM                         10
+#define SHOOT_SINGLE_CAMERA                 11
 
-#define HPERIOD                             10
+#define CONVERT_MS_TO_CYCLES(MS)            (MS * ((double)F_CPU / (double)1000000UL))
+
 #define NPULSES                             40
+#define HPERIOD                             CONVERT_MS_TO_CYCLES(10)
+#define SHUT_INSTANT                        CONVERT_MS_TO_CYCLES(7330)
+#define SHUT_DELAYED                        CONVERT_MS_TO_CYCLES(5360)
 
-#define SHUT_INSTANT                        7330
-#define SHUT_DELAYED                        5360
+
+static inline void _power_sleep() __attribute__((always_inline));
+#ifndef _USE_UTIL_DELAY
+static inline void _delay_loop_2(uint16_t __count) __attribute__((always_inline));
+#define delay_us(cycles) _delay_loop_2(cycles / 2)
+#endif
 
 void shoot_camera() {
     send_pulses();
-    _delay_us(SHUT_INSTANT);
+    delay_us(SHUT_INSTANT);
     send_pulses();
 }
 
@@ -74,7 +98,7 @@ void send_pulses() {
     uint8_t i = 0;
     for(i = 0; i < NPULSES; i++) {
         PORTB ^= (1 << LED_PIN);
-        _delay_us(HPERIOD);
+        delay_us(HPERIOD);
     }
     PORTB &= ~(1 << LED_PIN);
 }
@@ -87,41 +111,112 @@ void wdt_disable() {
     sei();
 }
 
-/*
-#define WDP3    5       16
-#define WDP2    2       4
-#define WDP1    1       2
-#define WDP0    0       1
-*/
-
-void wdt_enable() {
+void wdt_enable(uint8_t mode) {
     cli();
     MCUSR &= ~(1 << WDRF);
     WDTCR |= (1 << WDCE) | (1 << WDE);
-    WDTCR = (1 << WDIE) | (1 << WDP2);   // 0.25sec
+    WDTCR = (1 << WDIE) | mode;
     sei();
 }
 
-static uint8_t _digits[18] = {
-    SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HC) | (1 << _HD) | (1 << _HE) | (1 << _HF)),               // 0
-    SETUP_DIGIT((1 << _HB) | (1 << _HC)),                                                                   // 1
-    SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HG) | (1 << _HE) | (1 << _HD)),                            // 2
-    SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HG) | (1 << _HC) | (1 << _HD)),                            // 3
-    SETUP_DIGIT((1 << _HF) | (1 << _HG) | (1 << _HB) | (1 << _HC)),                                         // 4
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD)),                            // 5
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HE)),               // 6
-    SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HC)),                                                      // 7
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HE) | (1 << _HB)),  // 8
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HB)),               // 9
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HE) | (1 << _HB)),               // A
-    SETUP_DIGIT((1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HE)),                            // b
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HD) | (1 << _HE)),                                         // C
-    SETUP_DIGIT((1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HB) | (1 << _HE)),                            // d
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HD) | (1 << _HE)),                            // E
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HE)),                                         // F
-    SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HD) | (1 << _HE) | (1 << _HC)),                            // G
-    SETUP_DIGIT((1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HE) | (1 << _HB)),                            // H
-}; 
+struct interval_duration {
+    uint8_t     digit;
+    uint8_t     interval;
+    uint8_t     wdt;
+};
+
+static interval_duration durations[] = {
+    {   // 0
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HC) | (1 << _HD) | (1 << _HE) | (1 << _HF)),
+        .interval   = 0,
+        .wdt        = 0
+    },
+    {   // 1
+        .digit      = SETUP_DIGIT((1 << _HB) | (1 << _HC)),
+        .interval   = 1,
+        .wdt        = (1 << WDP2) | (1 << WDP1)
+    },
+    {   // 2
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HG) | (1 << _HE) | (1 << _HD)),
+        .interval   = 1,
+        .wdt        = (1 << WDP0) | (1 << WDP1) | (1 << WDP2)
+    },
+    {   // 3
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HG) | (1 << _HC) | (1 << _HD)),
+        .interval   = 3,
+        .wdt        = (1 << WDP2) | (1 << WDP1)
+    },
+    {   // 4
+        .digit      = SETUP_DIGIT((1 << _HF) | (1 << _HG) | (1 << _HB) | (1 << _HC)),
+        .interval   = 1,
+        .wdt        = (1 << WDP3)
+    },
+    {   // 5
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD)),
+        .interval   = 5,
+        .wdt        = (1 << WDP2) | (1 << WDP1)
+    },
+    {   // 6
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HE)),
+        .interval   = 3,
+        .wdt        = (1 << WDP0) | (1 << WDP1) | (1 << WDP2)
+    },
+    {   // 7
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HB) | (1 << _HC)),
+        .interval   = 7,
+        .wdt        = (1 << WDP2) | (1 << WDP1)
+    },
+    {   // 8
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HE) | (1 << _HB)),
+        .interval   = 1,
+        .wdt        = (1 << WDP0)
+    },
+    {   // 9
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HB)),
+        .interval   = 9,
+        .wdt        = (1 << WDP2) | (1 << WDP1)
+    },
+    {   // a 1
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HE) | (1 << _HB)),
+        .interval   = 15,
+        .wdt        = (1 << WDP3)
+    },
+    {   // b 2
+        .digit      = SETUP_DIGIT((1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HE)),
+        .interval   = 15,
+        .wdt        = (1 << WDP0)
+    },
+    {   // c 3
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HD) | (1 << _HE)),
+        .interval   = 45,
+        .wdt        = (1 << WDP3)
+    },
+    {   // d 4
+        .digit      = SETUP_DIGIT((1 << _HG) | (1 << _HC) | (1 << _HD) | (1 << _HB) | (1 << _HE)),
+        .interval   = 30,
+        .wdt        = (1 << WDP0)
+    },
+    {   // e 5
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HD) | (1 << _HE)),
+        .interval   = 75,
+        .wdt        = (1 << WDP3)
+    },
+    {   // f 6
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HG) | (1 << _HE)),
+        .interval   = 45,
+        .wdt        = (1 << WDP0)
+    },
+    {   // g 7
+        .digit      = SETUP_DIGIT((1 << _HA) | (1 << _HF) | (1 << _HD) | (1 << _HE) | (1 << _HC)),
+        .interval   = 105,
+        .wdt        = (1 << WDP3)
+    },
+    {   // h 8
+        .digit      = SETUP_DIGIT((1 << _HF) | (1 << _HG) | (1 << _HC) | (1 << _HE) | (1 << _HB)),
+        .interval   = 60,
+        .wdt        = (1 << WDP0)
+    },
+};
 
 void shift(uint8_t data, uint8_t flash) {
     if (flash) {
@@ -151,13 +246,14 @@ ISR(PCINT0_vect) {
 ISR(WDT_vect) {}
 
 int main() {
-    wdt_enable();
+    wdt_enable(WDT_DEFAULT);
     DDRB = 0xFF & ~(1 << BUTTON_PIN);
     PORTB = 0x00 | (1 << BUTTON_PIN);
     MAKE_LOW(ADCSRA, ADEN);                 // turn off ADC
     MAKE_LOW(ACSR, ACD);                    // turn off Analog Comparator
 #ifdef __AVR_ATtiny45__
     power_adc_disable();
+    power_timer0_disable();
     power_timer1_disable();
     power_usi_disable();
 #endif
@@ -165,32 +261,47 @@ int main() {
     sei();
     MAKE_HIGH(GIMSK, PCIE);                 // enable global pc interrupts
 
+#ifdef _USE_EEPROM_
+    _data = eeprom_read_byte(&_saved_data);
+    if (_data == 0xFF) { _data = 0x00; }
+#endif //_USE_EEPROM_
+
     SET_MODE(DISPLAY_VALUE);
 
     while (true) {
         if (IS_MODE(BUTTON_MODE)) {
-            if (++_data >= (sizeof(_digits) / sizeof(uint8_t))) {
+            if (++_data >= (sizeof(durations) / sizeof(struct interval_duration))) {
                 _data = 0;
             }
             _flash_cnt = 0;
             SET_MODE(TURN_ON_SR_LED);
             CLEAR_MODE(BUTTON_MODE);
             CLEAR_MODE(COUNT_TO_DISPLAY_OFF);
+            CLEAR_MODE(RUN_PROGRAM);
+            _program_cnt = 0;
         }
         if (IS_MODE(TURN_ON_SR_LED)) {
             // turn on Shift Register and LED
             SET_MODE(DISPLAY_VALUE);
             CLEAR_MODE(TURN_ON_SR_LED);
-            MAKE_LOW(PORTB, LED_PIN);
+            wdt_enable(WDT_DEFAULT);
         }
         if (IS_MODE(TURN_OFF_SR_LED)) {
             // turn off Shift Register and LED
+            shift(0xFF, 0);
             CLEAR_MODE(TURN_OFF_SR_LED);
-            MAKE_HIGH(PORTB, LED_PIN);
+            if (_data) {
+                SET_MODE(RUN_PROGRAM);
+            } else {
+                wdt_disable();
+            }
+#ifdef _USE_EEPROM_
+            eeprom_write_byte(&_saved_data, _data);
+#endif //_USE_EEPROM_
         }
         if (IS_MODE(COUNT_TO_DISPLAY_OFF)) {
             _display_timeout++;
-            if (_display_timeout >= 40) {   // 5 seconds
+            if (_display_timeout >= 20) {   // 5 seconds
                 _display_timeout = 0;
                 SET_MODE(TURN_OFF_SR_LED);
                 CLEAR_MODE(DISPLAY_VALUE);
@@ -198,21 +309,20 @@ int main() {
             }
         }
         if (IS_MODE(DISPLAY_VALUE)) {
-            shift(_digits[_data], IS_MODE(FLASHED_VALUE));
+            shift(durations[_data].digit, IS_MODE(FLASHED_VALUE));
             CLEAR_MODE(DISPLAY_VALUE);
             if (IS_MODE(TURN_OFF_FLASH)) {
                 CLEAR_MODE(TURN_OFF_FLASH);
                 CLEAR_MODE(FLASH_VALUE);
                 SET_MODE(COUNT_TO_DISPLAY_OFF);
+                _display_timeout = 0;
             } else {
                 SET_MODE(FLASH_VALUE);
             }
         }
         if (IS_MODE(FLASH_VALUE)) {
-            if ((_flash_cnt % 2) == 0) {
-                TOGGLE_BIT(_app_state, FLASHED_VALUE);
-            }
-            if (++_flash_cnt >= 30) {
+            TOGGLE_BIT(_app_state, FLASHED_VALUE);
+            if (++_flash_cnt >= 20) {
                 _flash_cnt = 0;
                 SET_MODE(TURN_OFF_FLASH);
                 CLEAR_MODE(FLASH_VALUE);
@@ -220,14 +330,48 @@ int main() {
             }
             SET_MODE(DISPLAY_VALUE);
         }
-/*
         if (IS_MODE(SHOOT_CAMERA)) {
             shoot_camera();
             CLEAR_MODE(SHOOT_CAMERA);
+            _program_cnt = 0;
         }
-*/
-        set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode
-        sleep_mode(); //Go to sleep
-      }
+        if (IS_MODE(SHOOT_SINGLE_CAMERA)) {
+            shoot_camera();
+            CLEAR_MODE(SHOOT_SINGLE_CAMERA);
+        }
+        _power_sleep();
+        if (IS_MODE(RUN_PROGRAM)) {
+            _program_cnt++;
+            if (_program_cnt >= durations[_data].interval) {
+                _program_cnt = 0;
+                SET_MODE(SHOOT_CAMERA);
+            }
+        }
+    }
     return 0;
+}
+
+#ifndef _USE_UTIL_DELAY
+void _delay_loop_2(uint16_t __count) {
+    __asm__ volatile (
+        "1: sbiw %0,1" "\n\t"
+        "brne 1b"
+            : "=w" (__count)
+            : "0" (__count)
+    );
+}
+#endif
+
+void _power_sleep() {
+    if (IS_MODE(RUN_PROGRAM)) {
+        wdt_enable(durations[_data].wdt);
+    }
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    cli();
+    sleep_enable();
+    sleep_bod_disable();
+    sei();
+    sleep_cpu();
+    sleep_disable();
+    wdt_enable(WDT_DEFAULT);
 }
